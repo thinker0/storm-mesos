50a833f87cfc8fe4c2d153f3baf6732740d34780
Erich Nachbar <erich@stuffylab.com>
2013-11-18 15:47:56 -0800

    Changed build file to build against standard mesos 0.14.2 and storm 0.9.0-rc3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-mesos "airbnb"
+(defproject storm/storm-mesos "0.9-rc3"
   :description "Storm integration with the Mesos cluster manager"
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
@@ -6,9 +6,9 @@
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
   :repositories {"mesosphere-public-repo" "http://s3.amazonaws.com/mesosphere-maven-public"}
   :dependencies [
-    [org.apache.mesos/mesos "0.14.0"]
+    [org.apache.mesos/mesos "0.14.2"]
     [com.google.protobuf/protobuf-java "2.4.1"]
-    [storm-core "0.9.0-rc3"]
+    [storm "0.9.0-rc3"]
     [org.apache.commons/commons-exec "1.1"]
     [commons-lang "2.6"]
     [com.googlecode.json-simple/json-simple "1.1"]
