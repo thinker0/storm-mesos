0f4a3b3fcc67b1b23bfa4810fe3f649ad5865cf7
Erich Nachbar <erich@stuffylab.com>
2013-12-05 14:58:17 -0800

    updating to final storm 0.9 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-mesos "0.9-rc3"
+(defproject storm/storm-mesos "0.9"
   :description "Storm integration with the Mesos cluster manager"
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
@@ -8,7 +8,7 @@
   :dependencies [
     [org.apache.mesos/mesos "0.14.2"]
     [com.google.protobuf/protobuf-java "2.4.1"]
-    [storm "0.9.0-rc3"]
+    [storm "0.9.0"]
     [org.apache.commons/commons-exec "1.1"]
     [commons-lang "2.6"]
     [com.googlecode.json-simple/json-simple "1.1"]
